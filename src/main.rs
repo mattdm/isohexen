6aaa822a32c74c68ed1482d1769840dcb849fe08
Matthew Miller <mattdm@mattdm.org>
2017-12-22 21:00:50 -0500

    let's try piston

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow =
+        WindowSettings::new("Island Zero", [1280, 720])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics| {
+            // stuff goes here
+        });
+    }
 }

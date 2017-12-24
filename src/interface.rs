a8a23ea9785d572e2c77069666fc481471cdaa87
Matthew Miller <mattdm@mattdm.org>
2017-12-23 22:49:54 -0500

    well, that's something

--- src/interface.rs
+++ src/interface.rs
@@ -9,6 +9,7 @@ use sdl2::keyboard::Keycode;
 use sdl2::image::LoadTexture;
 use sdl2::rect::Rect;
 
+use sdl2::pixels::Color;
 
 use std::time;
 use std::thread;
@@ -21,7 +22,13 @@ pub fn gameloop(canvas: &mut sdl2::render::WindowCanvas, event_pump: &mut sdl2::
     let texture_creator = canvas.texture_creator();
     let block_texture = texture_creator.load_texture("/home/mattdm/misc/island/images/hexblocks.png").unwrap();
 
+    canvas.set_draw_color(Color::RGB(0,32,128));
     canvas.clear();
+
+    canvas.copy(&block_texture, Rect::new(0,0,256,256), Rect::new(608,368,64,64)).expect("Render failed");
+    canvas.copy(&block_texture, Rect::new(0,0,256,256), Rect::new(656,384,64,64)).expect("Render failed");
+    canvas.copy(&block_texture, Rect::new(0,0,256,256), Rect::new(608,400,64,64)).expect("Render failed");
+
     canvas.present();
     
     let mut i = 0;
@@ -37,18 +44,21 @@ pub fn gameloop(canvas: &mut sdl2::render::WindowCanvas, event_pump: &mut sdl2::
                     i=i+1;
                     if i>5 {
                         i=0
-                    }
+                    };
+                    canvas.copy(&block_texture, Rect::new(i*256,0,256,256), Rect::new(608,368,64,64)).expect("Render failed");
+                    canvas.copy(&block_texture, Rect::new(i*256,0,256,256), Rect::new(656,384,64,64)).expect("Render failed");
+                    canvas.copy(&block_texture, Rect::new(i*256,0,256,256), Rect::new(608,400,64,64)).expect("Render failed");
+                    canvas.present();
                 },
                 _ => {}
             }
         }
         
         // The rest of the game loop goes here...
-        canvas.copy(&block_texture, Rect::new(i*256,0,256,256), Rect::new(608,400,64,64)).expect("Render failed");
+        
         
         
 
-        canvas.present();
 
         thread::sleep(time::Duration::from_millis(10));
     }

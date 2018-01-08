c4681cc9a0983242d5c6eea640018cd5d97ba1d6
Matthew Miller <mattdm@mattdm.org>
2018-01-07 19:32:45 -0500

    I think I have the lifetime stuff figured out

--- src/sprite.rs
+++ src/sprite.rs
@@ -1,7 +1,9 @@
 //use sdl2::rect::Point;
 use std::collections::HashMap;
+use std::path;
 
 use sdl2::render;
+use sdl2::image::LoadTexture;
 
 #[derive(Clone,Copy,Debug)]
 pub struct Sprite<'a> {
@@ -21,16 +23,16 @@ impl<'a> Sprite<'a> {
 
 pub struct SpriteAtlas<'a> {
     pub sprite: HashMap<&'a str,Sprite<'a>>,
-    sprite_sheet: &'a render::Texture<'a>,
+    sprite_sheet: render::Texture<'a>,
 }
 
 impl<'a> SpriteAtlas<'a> {
     // FIXME: instead of hard-coding all this stuff, 
     // read from a description file
-    pub fn new(sprite_sheet: &'a render::Texture) -> SpriteAtlas<'a> {
+    pub fn new(texture_creator: &'a render::TextureCreator<render::Texture>) -> SpriteAtlas<'a> {
         SpriteAtlas {
             sprite: HashMap::new(),
-            sprite_sheet,
+            sprite_sheet: texture_creator.load_texture(path::Path::new("images/spritesheet.png")).unwrap(),
         }
     }
 }

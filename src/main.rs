6d393abf944be1b84b348b0ce107ddd72bc0cdd6
Matthew Miller <mattdm@mattdm.org>
2018-01-15 17:43:21 -0500

    argument parsing for fullscreen

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate sdl2;
 extern crate rand;
 extern crate toml;
+extern crate docopt;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
 //use std::thread;
+use docopt::Docopt;
 
 mod interface;
 mod direction;
@@ -13,17 +15,30 @@ mod hexgeometry;
 mod landscape;
 mod sprite;
 
+
+const USAGE: &'static str = "
+IsoHexEn Island Demo
+
+Usage: isohexen [options]
+
+-f, --fullscreen  Start Full Screen
+";
+
 pub fn main() {
+
+    let args = Docopt::new(USAGE)
+                      .and_then(|dopt| dopt.parse())
+                      .unwrap_or_else(|e| e.exit());
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let _image_context = sdl2::image::init(sdl2::image::INIT_PNG).unwrap();
 
-    let window = video_subsystem.window("Little Island", 960, 540)
-        //.fullscreen_desktop()
-        //.resizable()
-        .build()
-        .unwrap();
+    let mut window_builder = video_subsystem.window("Little Island", 960, 540);
+    if args.get_bool("--fullscreen") {
+        window_builder.fullscreen_desktop();
+    }
+    let window = window_builder.build().unwrap();
  
     let mut canvas = window.into_canvas()
         .target_texture()
